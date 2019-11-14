582d6a6fcd93f7702819fd7db1eed6c106ba4cd2
Tamagna Davide <supernikx@gmail.com>
2019-11-14 14:11:51 +0100

    Background che scorre da sistemare ma funzionante

---
+++ Assets/Scripts/Background/BackgroundController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    Vector3 startPos;
+    float screenHeight;
+    Bounds bound;
+    BackgroundManager bgManager;
+    Camera cam;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    public void Setup(BackgroundManager _bgMng, Camera _cam)
+    {
+        bgManager = _bgMng;
+        cam = _cam;
+        bound = GetComponent<SpriteRenderer>().bounds;
+
+        screenHeight = cam.orthographicSize - cam.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (bgManager == null || !bgManager.GetCanMove())
+            return;
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.down, bgManager.GetMovementSpeed());
+        if (CheckScreenPosition())
+            bgManager.RespawnBG(this);
+    }
+
+    private bool CheckScreenPosition()
+    {
+        if (bound.extents.y + transform.position.y < -screenHeight)
+            return true;
+        return false;
+    }
+
+    public void ResetBG()
+    {
+        transform.position = startPos;
+    }
+}
---
+++ Assets/Scripts/Background/BackgroundManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject startBg;
+    [SerializeField]
+    private BackgroundController bg1;
+    [SerializeField]
+    private BackgroundController bg2;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float backgoundDislpace = 11.5f;
+    [SerializeField]
+    private float maxStartPosY;
+
+    private bool canMove = false;
+    private Camera cam;
+    private Vector3 startPos;
+    private BackgroundController currentBG;
+    private IEnumerator backgroundStartRoutine;
+
+    public void Setup(Camera _cam)
+    {
+        startPos = transform.position;
+        currentBG = bg1;
+        cam = _cam;
+    }
+
+    public void StartBackground()
+    {
+        backgroundStartRoutine = BackgroundStartCoroutine();
+        StartCoroutine(backgroundStartRoutine);
+    }
+
+    public void ResetBackground()
+    {
+        canMove = false;
+        if (backgroundStartRoutine != null)
+            StopCoroutine(backgroundStartRoutine);
+
+        transform.position = startPos;
+        bg1.ResetBG();
+        bg2.ResetBG();
+    }
+
+    public Bounds GetBackgroundBounds()
+    {
+        return bg1.GetComponent<SpriteRenderer>().bounds;
+    }
+
+    private IEnumerator BackgroundStartCoroutine()
+    {
+        while (transform.position.y > maxStartPosY)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.down, moveSpeed);
+            yield return new WaitForFixedUpdate();
+        }
+
+        bg1.Setup(this, cam);
+        bg2.Setup(this, cam);
+        canMove = true;
+    }
+
+    public void RespawnBG(BackgroundController bg)
+    {
+        if (bg != currentBG)
+            return;
+        BackgroundController other = (bg == bg1) ? bg2 : bg1;
+        bg.transform.position = new Vector3(other.transform.position.x, other.transform.position.y + backgoundDislpace, other.transform.position.z);
+        currentBG = other;
+    }
+
+    public float GetMovementSpeed()
+    {
+        return moveSpeed;
+    }
+
+    public bool GetCanMove()
+    {
+        return canMove;
+    }
+}
